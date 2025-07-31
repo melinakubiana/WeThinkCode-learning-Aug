ec360e048267a6737b51cfc6f156f86e5e09f053
madishego <melinakubiana@gmail.com>
2025-07-31 22:15:43 +0000

    6

---
+++ deep.py
@@ -0,0 +1,8 @@
+answer= input('What is the answer to the Great Question of Life, the Universe and Everything?')
+dash= 'forty-two'
+no= 'forty two'
+
+if answer== 42 or dash or no:
+    print ('Yess!')
+else:
+    print ('No')
